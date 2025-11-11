db8ff24d5c14e9cb1835482d5a29dc9e7cda8b65
Nane <nanekode@gmail.com>
2025-11-11 09:14:29 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,35 @@
+import math
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
+
 def add(a, b): 
-    pass
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError as e:
+        print(str(e))
+
+def log(a,b):
+    try:
+        return math.log(b,a)
+    except ValueError as e:
+        print(str(e))]
+
+def exp(a,b):
+    return a**b
+
 
 
